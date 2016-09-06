369893e833a56085b9649717e50dba91c0256b08
huangjilaiqin <1577594730@qq.com>
2016-09-06 15:17:42 +0800

    第一次学习看到的界面

--- app/src/main/java/cn/lessask/word/word/MainActivity.java
+++ app/src/main/java/cn/lessask/word/word/MainActivity.java
@@ -1,9 +1,13 @@
 package cn.lessask.word.word;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +18,29 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,WordActivity.class);
+                Intent intent = new Intent(MainActivity.this, WordActivity.class);
                 startActivity(intent);
             }
         });
+
+        WaveView mWaveView = (WaveView)findViewById(R.id.waveview);
+        /*
+        mWaveView.setDuration(5000);
+        mWaveView.setSpeed(1000);
+        mWaveView.setInitialRadius(150);
+        mWaveView.setMaxRadius(500);
+        mWaveView.setColor(Color.parseColor("#ff0000"));
+        mWaveView.setInterpolator(new LinearOutSlowInInterpolator());
+        //mWaveView.setInterpolator(new AccelerateInterpolator(1.2f));
+        mWaveView.start();
+        */
+
+        mWaveView.setDuration(5000);
+        mWaveView.setSpeed(1000);
+        mWaveView.setStyle(Paint.Style.FILL_AND_STROKE);
+        mWaveView.setColor(getResources().getColor(R.color.hublue));
+        //mWaveView.setColor(Color.RED);
+        mWaveView.setInterpolator(new LinearOutSlowInInterpolator());
+        mWaveView.start();
     }
 }
--- app/src/main/java/cn/lessask/word/word/WordActivity.java
+++ app/src/main/java/cn/lessask/word/word/WordActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import at.grabner.circleprogress.AnimationState;
@@ -17,6 +19,10 @@ public class WordActivity extends AppCompatActivity {
     View wordFirst;
 
     private CircleProgressView timer;
+    private LinearLayout meanings;
+    private View nextLayout,selectLayout;
+    private Button know,unknow,next;
+    private int thinkTimes = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,36 +30,58 @@ public class WordActivity extends AppCompatActivity {
         LayoutInflater inflater = LayoutInflater.from(this);
         //以上两行功能一样
         wordFirst = inflater.inflate(R.layout.word_first, null);
-        setContentView(wordFirst);
 
-        timer = (CircleProgressView) wordFirst.findViewById(R.id.timer);
-        timer.setValueAnimated(100,2000);
-        timer.setSeekModeEnabled(false);
-        timer.setTextMode(TextMode.TEXT);
-        timer.setText("");
-
-        timer.setOnAnimationStateChangedListener(new AnimationStateChangedListener() {
-
+        nextLayout=wordFirst.findViewById(R.id.next_layout);
+        selectLayout=wordFirst.findViewById(R.id.select_layout);
+        know = (Button)wordFirst.findViewById(R.id.know);
+        know.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAnimationStateChanged(AnimationState _animationState) {
-                System.out.println(_animationState);
-                switch (_animationState){
-                    case START_ANIMATING_AFTER_SPINNING:
-                        System.out.println("START_ANIMATING_AFTER_SPINNING");
-                        break;
-                    case SPINNING:
-                        System.out.println("SPINNING");
-                        break;
-                    case END_SPINNING:
-                        System.out.println("END_SPINNING");
-                        break;
-                    case END_SPINNING_START_ANIMATING:
-                        System.out.println("END_SPINNING_START_ANIMATING");
-                        break;
-
-                }
+            public void onClick(View v) {
+                selectLayout.setVisibility(View.INVISIBLE);
+                nextLayout.setVisibility(View.VISIBLE);
+                timer.setVisibility(View.INVISIBLE);
+                meanings.setVisibility(View.VISIBLE);
+            }
+        });
+        unknow = (Button)wordFirst.findViewById(R.id.unknow);
+        unknow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectLayout.setVisibility(View.INVISIBLE);
+                nextLayout.setVisibility(View.VISIBLE);
+                timer.setVisibility(View.INVISIBLE);
+                meanings.setVisibility(View.VISIBLE);
+            }
+        });
+        next = (Button)wordFirst.findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
             }
         });
 
+        meanings = (LinearLayout)wordFirst.findViewById(R.id.meanings);
+        timer = (CircleProgressView) wordFirst.findViewById(R.id.timer);
+        timer.setValueAnimated(100, thinkTimes);
+        timer.setSeekModeEnabled(false);
+        timer.setTextMode(TextMode.TEXT);
+        timer.setText("");
+        timer.setSpinSpeed(1);
+
+        setWordFirstLayout();
+
+
+    }
+
+    private void setWordFirstLayout(){
+        setContentView(wordFirst);
+        timer.setVisibility(View.VISIBLE);
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        timer.setVisibility(View.INVISIBLE);
+                        meanings.setVisibility(View.VISIBLE);
+                    }
+                }, thinkTimes + 200);
     }
 }
